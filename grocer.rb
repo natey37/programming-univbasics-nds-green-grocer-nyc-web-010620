6c492666fbd38bb13970698d1f59d4136d5992fc
Nathan Brickett <natey37@gmail.com>
2019-12-18 23:11:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,7 +77,7 @@ def apply_clearance(cart)
   while index < cart.count do 
     item = cart[index]
     if item[:clearance]
-      discounted_price = item[:price] * 0.8
+      discounted_price = (item[:price] * 0.8).round(2)
       item[:price] = discounted_price
     end 
     index += 1 
